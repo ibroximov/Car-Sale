dfdcb30c842d160749794133837f4ab5550d76bf
ibroximov <114992129+ibroximov@users.noreply.github.com>
2023-02-17 18:38:08 +0300

    commit car sale man

---
+++ main.js
@@ -0,0 +1,29 @@
+let search = document.querySelector('.search__box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    menu.classList.remove('active');
+    
+} 
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+    search.classList.remove('active');
+} 
+
+// Hide menu and search box on scroll
+window.onscroll = () =>  {
+    menu.classList.remove('active');
+    search.classList.remove('active');
+}
+
+
+
+// Header
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , () => {
+    header.classList.toggle('shadow', window.scrollY > 0 );
+});
